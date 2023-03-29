e2538e81e34c99e5431a4d101b32636570d45433
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-03-29 22:55:34 +0800

    chore: 🎨 format code

--- src/_includes/layouts/article.tmpl.ts
+++ src/_includes/layouts/article.tmpl.ts
@@ -1,12 +1,11 @@
 import type { PageData } from '../types.ts'
 
 export const layout = 'layouts/root.tmpl.ts'
-export default ({ content, summary, title }: PageData) => `
-  <main>
+export default ({ content, summary, title }: PageData) =>
+  `<main>
     <article class="h-entry">
       <h1 class="p-name">${title}</h1>
       ${summary ? `<meta class="p-summary" content=${summary}></meta>` : ''}
       <div class="e-content">${content}</div>
     </article>
-  </main>
-`
+  </main>`
--- src/_includes/layouts/root.tmpl.ts
+++ src/_includes/layouts/root.tmpl.ts
@@ -1,13 +1,12 @@
 import type { PageData, PageHelpers } from '../types.ts'
 import Head from '../templates/head.tmpl.ts'
 
-export default (data: PageData, helpers: PageHelpers) => `
-  <html lang="${data.metas?.lang ?? 'en'}">
+export default (data: PageData, helpers: PageHelpers) =>
+  `<html lang="${data.metas?.lang ?? 'en'}">
     <head prefix="og: https://ogp.me/ns#">
       ${Head(data, helpers)}
     </head>
     <body>
       ${data.content}
     </body>
-  </html>
-`
+  </html>`
