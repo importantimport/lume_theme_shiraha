b0647e654156027e01bd88afaac1b6e74205fbe1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-04-09 15:59:23 +0800

    feat: ✨ view transition

--- src/_includes/templates/head/meta.tmpl.ts
+++ src/_includes/templates/head/meta.tmpl.ts
@@ -6,6 +6,7 @@ export default ({ tmp }: PageData, _helpers: PageHelpers) =>
     html`<meta charset="utf-8" />`,
     html`<meta name="viewport" content="width=device-width, initial-scale=1.0" />`,
     html`<meta name="color-scheme" content="light dark" />`,
+    html`<meta name="view-transition" content="same-origin" />`,
     ...(tmp?.schemes
       ? [
         html`<meta name="theme-color" content="${tmp.schemes.light.primary}" media="(prefers-color-scheme: light)" />`,
