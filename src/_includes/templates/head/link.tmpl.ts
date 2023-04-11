baf806d21c0a5d9f36cd14267ca22f9f70af7fe7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-04-11 19:58:47 +0800

    fix(head/link): 🐛 use optional chaining

--- src/_includes/templates/head/link.tmpl.ts
+++ src/_includes/templates/head/link.tmpl.ts
@@ -3,5 +3,5 @@ import { html } from '../../utils/html.ts'
 
 export default ({ site }: PageData, _helpers: PageHelpers) =>
   [
-    html`<link rel="icon" href="${ site.icon ?? 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%22-.1em%22 y=%22.9em%22 font-size=%2290%22>❄️</text></svg>'}">`,
+    html`<link rel="icon" href="${ site?.icon ?? 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%22-.1em%22 y=%22.9em%22 font-size=%2290%22>❄️</text></svg>'}">`,
   ].join('')
