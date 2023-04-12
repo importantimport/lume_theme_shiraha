5db9b4d28b8ebecee9c9a0ae1154801f683f5291
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-04-12 20:36:54 +0800

    refactor(head/meta): 🔥 clean useless import

--- src/_includes/templates/head/meta.tmpl.ts
+++ src/_includes/templates/head/meta.tmpl.ts
@@ -1,7 +1,7 @@
 import type { PageData, PageHelpers } from '../../types.ts'
 import { html } from '../../utils/html.ts'
 
-export default ({ tmp }: PageData, _helpers: PageHelpers) =>
+export default (_data: PageData, _helpers: PageHelpers) =>
   [
     html`<meta charset="utf-8" />`,
     html`<meta name="viewport" content="width=device-width, initial-scale=1.0" />`,
