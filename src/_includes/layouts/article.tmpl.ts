e60191ffa114f2dd953ffdf83e3d041f0a0fa2c3
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-21 11:03:43 +0800

    refactor(layouts/article): ♻️ add container

--- src/_includes/layouts/article.tmpl.ts
+++ src/_includes/layouts/article.tmpl.ts
@@ -8,14 +8,16 @@ export const layout = 'layouts/post.tmpl.ts'
 export default (data: PageData, helpers: PageHelpers) =>
   html`<article class="h-entry">
     <h1 class="p-name">${data.title}</h1>
-    ${authors(data, helpers)}
-    ·
-    ${date(data, helpers)}
-    ${
+    <p>
+      ${authors(data, helpers)}
+      ·
+      ${date(data, helpers)}
+      ${
     data.summary
       ? `<meta class="p-summary" content=${data.summary}></meta>`
       : ''
   }
+    </p>
     <div class="e-content">${data.content}</div>
     ${tags(data, helpers)}
   </article>`
