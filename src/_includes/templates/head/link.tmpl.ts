6f66dfa9582f935408ae0439c0269a08a35853bb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-21 11:04:29 +0800

    feat: ✨ add view transition style

--- src/_includes/templates/head/link.tmpl.ts
+++ src/_includes/templates/head/link.tmpl.ts
@@ -4,4 +4,5 @@ import { html } from '../../utils/html.ts'
 export default ({ site }: PageData, { url }: PageHelpers) =>
   [
     html`<link rel="icon" href="${site.icon}">`,
+    html`<link rel="stylesheet" href="${url('/styles.css')}" />`,
   ].join('')
