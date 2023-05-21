5fad9c11b03e301305e5d67cd203646f8bc0e21f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-21 11:07:30 +0800

    refactor(head): ♻️ use shiraha nightly

--- src/_includes/templates/head/shiraha.tmpl.ts
+++ src/_includes/templates/head/shiraha.tmpl.ts
@@ -1,7 +1,6 @@
 import type { PageData, PageHelpers } from '../../types.ts'
 import { html } from '../../utils/html.ts'
 
-// TODO: Remove Baseline Theme
 export default (data: PageData, _helpers: PageHelpers) =>
   [
     ...(data.shiraha
@@ -9,13 +8,13 @@ export default (data: PageData, _helpers: PageHelpers) =>
         html`<script>window.shiraha = ${JSON.stringify(data.shiraha)}</script>`,
       ]
       : []),
-    ...(data.shiraha.colors !== false
+    ...(data.shiraha?.colors
       ? [
         html`<script type="module" src="https://esm.run/shiraha-colors" async></script>`,
       ]
       : []),
-    html`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/shiraha/dist/shiraha.css" />`,
-    html`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/shiraha/dist/themes/yuki.css" />`,
+    // html`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/shiraha/dist/shiraha.css" />`,
+    // html`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/shiraha/dist/themes/yuki.css" />`,
+    html`<link rel="stylesheet" href="https://shiraha.js.org/nightly/shiraha.css" />`,
+    html`<link rel="stylesheet" href="https://shiraha.js.org/nightly/themes/baseline-tone-based.css" />`,
   ].join('')
-
-// <script type="module" src="https://cdn.jsdelivr.net/npm/shiraha-colors@0.0.2/dist/index.js"></script>
