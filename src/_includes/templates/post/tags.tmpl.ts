e022e98f0e234e4d27e28361d1b38943579832b2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-29 15:12:41 +0800

    refactor(post/tags): ♻️ add container

--- src/_includes/templates/post/tags.tmpl.ts
+++ src/_includes/templates/post/tags.tmpl.ts
@@ -2,9 +2,13 @@ import type { PageData, PageHelpers } from '../../types.ts'
 import { html } from '../../utils/html.ts'
 
 export default ({ tags }: PageData, { url }: PageHelpers) =>
-  tags?.map((tag) =>
-    html`<span class="chip chip-tonal">
+  html`<p>
+    ${
+    tags?.map((tag) =>
+      html`<span class="chip chip-tonal">
       <span class="material-symbols-outlined">tag</span>
       <a href="${url(`/t/${tag}`)}" class="p-category">${tag}</a>
     </span>`
-  ).join('')
+    ).join('')
+  }
+  </p>`
