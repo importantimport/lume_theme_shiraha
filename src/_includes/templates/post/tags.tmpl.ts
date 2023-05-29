7bbea399a6e60e42ee0e184bb76bbf81572716b4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-29 15:12:06 +0800

    refactor(post/tags): ♻️ update template

--- src/_includes/templates/post/tags.tmpl.ts
+++ src/_includes/templates/post/tags.tmpl.ts
@@ -3,8 +3,8 @@ import { html } from '../../utils/html.ts'
 
 export default ({ tags }: PageData, { url }: PageHelpers) =>
   tags?.map((tag) =>
-    html`<span>#<a
-      href="${url(`/t/${tag}`)}"
-      class="p-category">${tag}</a>
+    html`<span class="chip chip-tonal">
+      <span class="material-symbols-outlined">tag</span>
+      <a href="${url(`/t/${tag}`)}" class="p-category">${tag}</a>
     </span>`
   ).join('')
