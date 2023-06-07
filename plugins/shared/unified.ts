a591ad8aebcd56597ca81b46a769fa3819f5ef15
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-06-07 14:11:40 +0800

    refactor(plugins): ♻️ import from npm

--- plugins/shared/unified.ts
+++ plugins/shared/unified.ts
@@ -1,7 +1,7 @@
 // remark plugins
-import remarkFFF from 'https://esm.sh/remark-fff'
+import remarkFFF from 'npm:remark-fff'
 // rehype plugins
-import rehypeStarryNight from 'https://esm.sh/@microflash/rehype-starry-night'
+import rehypeStarryNight from 'npm:@microflash/rehype-starry-night'
 
 export const remarkPlugins = [
   [
