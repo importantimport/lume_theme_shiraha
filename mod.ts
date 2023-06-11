ed8f18fbcef5cc85f98ff6a25728fe9a84d95d98
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-06-11 22:34:51 +0800

    refactor(mod)!: 💥 rename exports

--- mod.ts
+++ mod.ts
@@ -1,19 +1,12 @@
 import type { Site } from 'lume/core.ts'
 // import { expandGlobSync } from 'fs/expand_glob.ts'
 import sharedPlugins from './plugins/shared.ts'
+import remarkPlugins from './plugins/remark.ts'
 
 const src = (files: string[]) => (site: Site) =>
   files.forEach((file) =>
     site.remoteFile(file, import.meta.resolve(`./src/${file}`))
   )
-
-export const includes = () =>
-  src([
-    '_includes/layouts/root.tmpl.ts',
-    '_includes/layouts/post.tmpl.ts',
-    '_includes/layouts/list.tmpl.ts',
-    '_includes/layouts/article.tmpl.ts',
-  ])
 
 export const archetypes = () => src(['_archetypes/article.ts'])
 
@@ -24,6 +17,14 @@ export const data = () =>
     '_data/metas.yml',
     '_data/shiraha.yml',
     '_data/site.yml',
+  ])
+
+export const includes = () =>
+  src([
+    '_includes/layouts/root.tmpl.ts',
+    '_includes/layouts/post.tmpl.ts',
+    '_includes/layouts/list.tmpl.ts',
+    '_includes/layouts/article.tmpl.ts',
   ])
 
 export const pages = () =>
@@ -38,22 +39,31 @@ export const pages = () =>
     'tags.tmpl.ts',
   ])
 
-export const ltsSrc = () => (site: Site) =>
+export const templates = () => (site: Site) =>
   site.use(includes()).use(archetypes()).use(data()).use(pages())
 
-export const lts = () => (site: Site) => site.use(ltsSrc()).use(sharedPlugins())
+export const lts = () => (site: Site) =>
+  site.use(templates()).use(sharedPlugins()).use(remarkPlugins())
 
 // for (const file of expandGlobSync('./src/_includes/*.ts')) {
 //   console.log(file.path)
 //   site.remoteFile(file.path.slice(6), import.meta.resolve(file.path))
 // }
 
-// default: lts (Src + Plugins)
-// ltsSrc (Src)
-// ltsMDX (Src + Plugins + JSX&MDX)
-// includes (_includes)
+// --- templates ---
 // archetypes (_archetypes)
 // data (_data)
+// includes (_includes)
 // pages (*.tmpl.ts)
+// *templates* (archetypes + data + includes + pages)
+
+// --- plugins ---
+// base (plugins/shared)
+// remark (plugins/remark)
+// mdx (plugins/mdx) (TODO)
+// extra (plugins/extra) (TODO)
+
+// --- default ---
+// lts (templates + plugins/base + plugins/remark)
 
 export default lts
