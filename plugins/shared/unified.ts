e117b840179e2ef4ce47f7ff30bea0f3a74193fe
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-12-12 16:42:02 +0800

    chore: add comments

--- plugins/shared/unified.ts
+++ plugins/shared/unified.ts
@@ -3,6 +3,9 @@ import remarkFFF from 'https://esm.sh/remark-fff'
 import remarkReadingTime from 'npm:remark-reading-time@2.0.1'
 // rehype plugins
 import rehypePrettyCode from 'npm:rehype-pretty-code@0.12.1'
+// shikiji transformers
+// import { transformerNotationDiff } from 'npm:shikiji-transformers@0.8.3'
+// import { transformerTwoSlash } from 'npm:shikiji-twoslash@0.8.3'
 
 export const remarkPlugins = [
   remarkReadingTime,
@@ -27,6 +30,10 @@ export const rehypePlugins = [
         light: 'github-light',
         dark: 'github-dark',
       },
+      // transformers: [
+      //   transformerNotationDiff(),
+      //   transformerTwoSlash(),
+      // ],
     },
   ],
 ]
