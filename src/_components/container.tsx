ea901af0b7304c712fc61880542728e2e18d1592
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-12-14 21:30:21 +0800

    fix: mf2 class name

--- src/_components/container.tsx
+++ src/_components/container.tsx
@@ -1,4 +1,8 @@
-export default ({ comp: { Footer, Header }, children, className }: Lume.Data) => (
+export type Props = {
+  className: 'h-entry' | 'h-feed'
+}
+
+export default ({ comp: { Footer, Header }, children, className }: Lume.Data & Props) => (
   <main class={`${className} w-full max-w-screen-md mx-auto p-8 flex flex-col gap-4`}>
     <Header />
     {children}
--- src/_includes/layouts/article.tsx
+++ src/_includes/layouts/article.tsx
@@ -1,16 +1,18 @@
 export const layout = 'layouts/body.tsx'
-export default ({ children, comp: { Container }, date, title, url, readingTime }: Lume.Data, { slug }: Lume.Helpers) => (
+export default ({ children, comp: { Container }, date, title, url: dataUrl, readingTime }: Lume.Data, { url, slug }: Lume.Helpers) => (
   <Container className="h-entry">
     <article>
-      <h1 class="p-name" style={`--name: article-title-${slug(url)}`}>{title}</h1>
-      <p class="lts-date" style={`--name: article-date-${slug(url)}`}>
-        <time class="dt-published" datetime={date.toISOString()}>
-          {date.toLocaleDateString()}
-        </time>
+      <h1 class="p-name" style={`--name: article-title-${slug(dataUrl)}`}>{title}</h1>
+      <p class="lts-date" style={`--name: article-date-${slug(dataUrl)}`}>
+        <a class="u-url u-uid" href={url(dataUrl)}>
+          <time class="dt-published" datetime={date.toISOString()}>
+            {date.toLocaleDateString()}
+          </time>
+        </a>
         <span> · </span>
         <span>{readingTime.text}</span>
       </p>
-      <section class="e-content mt-12 flex flex-col gap-6 children:m-0" style={`--name: article-content-${slug(url)}`}>
+      <section class="e-content mt-12 flex flex-col gap-6 children:m-0" style={`--name: article-content-${slug(dataUrl)}`}>
         {children}
       </section>
     </article>
