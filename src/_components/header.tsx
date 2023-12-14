7ba37a720689be08bc650371cd671e08ffe96cb0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-12-14 22:33:11 +0800

    fix: mf2 class name

--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -1,7 +1,8 @@
 export default ({}: Lume.Data, { url }: Lume.Helpers) => (
   <header class="p-author h-card w-full h-12 flex place-items-center gap-4">
-    <img class="u-photo h-8 w-8 rounded-full" src="https://kwaa.dev/assets/maskable@192.webp" alt="藍+85CD" />
-    <a rel="author" class="p-name u-url no-underline" href="https://kwaa.dev">藍+85CD</a>
+    <data class="u-photo" value="https://kwaa.dev/assets/maskable@192.webp" />
+    <img class="h-8 w-8 rounded-full" src="https://kwaa.dev/assets/maskable@192.webp" alt="藍+85CD" />
+    <a rel="author" class="p-name u-url u-uid no-underline" href="https://kwaa.dev">藍+85CD</a>
     <div class="flex-1"></div>
     <a href={url('/')}>Home</a>
   </header>
