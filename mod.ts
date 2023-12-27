71f8e473ed0eaff9c401a553390a8177c3ca8a77
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-12-27 15:20:37 +0800

    fix: update entry point

--- mod.ts
+++ mod.ts
@@ -1,44 +1,59 @@
 import sharedPlugins from './plugins/shared.ts'
 import remarkPlugins from './plugins/remark.ts'
 
-const src = (files: string[]) => (site: Lume.Site) =>
+const src = (path = '/', files: string[]) => () => (site: Lume.Site) =>
   files.forEach((file) =>
-    site.remoteFile(file, import.meta.resolve(`./src/${file}`))
+    site.remoteFile(file, import.meta.resolve(`./src${path}${file}`))
   )
 
-export const archetypes = () => src(['_archetypes/article.ts'])
+export const archetypes = src('/_archetypes/', ['article.ts'])
 
-export const data = () =>
-  src([
-    '_data/authors.yml',
-    '_data/date.yml',
-    '_data/metas.yml',
-    '_data/shiraha.yml',
-    '_data/site.yml',
-  ])
+export const components = src('/_components/', [
+  'container.tsx',
+  'footer.tsx',
+  'head.tsx',
+  'header.tsx',
+  'tags.tsx',
+])
 
-export const includes = () =>
-  src([
-    '_includes/layouts/root.page.ts',
-    '_includes/layouts/post.page.ts',
-    '_includes/layouts/list.page.ts',
-    '_includes/layouts/article.page.ts',
-  ])
+export const data = src('/_data/', [
+  // '_data/authors.yml',
+  // '_data/date.yml',
+  // '_data/metas.yml',
+  // '_data/shiraha.yml',
+  'site.yml',
+])
 
-export const pages = () =>
-  src([
-    '404.md',
-    'categories.feed.json.page.ts',
-    'categories.page.ts',
-    'index.feed.json.page.ts',
-    'index.page.ts',
-    'styles.css.page.ts',
-    'tags.feed.json.page.ts',
-    'tags.page.ts',
-  ])
+export const includes = src('/_includes/', [
+  'layouts/article-list.tsx',
+  'layouts/article.tsx',
+  'layouts/body.tsx',
+  'layouts/feed.json.ts',
+  'styles/theme.dark.css',
+  'styles/theme.light.css',
+])
+
+export const pages = src('/', [
+  'index.feed.json.page.ts',
+  'index.page.ts',
+  'styles.css',
+  // '404.md',
+  // 'categories.feed.json.page.ts',
+  // 'categories.page.ts',
+  // 'index.feed.json.page.ts',
+  // 'index.page.ts',
+  // 'styles.css.page.ts',
+  // 'tags.feed.json.page.ts',
+  // 'tags.page.ts',
+])
 
 export const templates = () => (site: Lume.Site) =>
-  site.use(includes()).use(archetypes()).use(data()).use(pages())
+  site
+    .use(archetypes())
+    .use(components())
+    .use(data())
+    .use(includes())
+    .use(pages())
 
 export const lts = () => (site: Lume.Site) =>
   site.use(templates()).use(sharedPlugins()).use(remarkPlugins())
