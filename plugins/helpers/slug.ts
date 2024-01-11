1fc2a67f1c1e7b2a7d6b225649294e9fb52779d1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-11 17:07:51 +0800

    Revert "refactor(helpers/slug): use slugger"
    This reverts commit e116a37c2b4a013ae9b519d49f11d40348b0c0ec.

--- plugins/helpers/slug.ts
+++ plugins/helpers/slug.ts
@@ -1,8 +1,7 @@
-import GithubSlugger, { type slug } from 'npm:github-slugger@2.0.0'
+import { slug } from 'npm:github-slugger@2.0.0'
 
 export default () => (site: Lume.Site) => {
-  const slugger = new GithubSlugger()
-  site.helper('slug', slugger.slug, { type: 'tag' })
+  site.helper('slug', slug, { type: 'tag' })
 }
 
 /** Extends Data interface */
