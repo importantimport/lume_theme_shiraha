89b71ba2b04e29f63e1d09bcef58d7b92ab1efaf
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-11 20:36:53 +0800

    refactor: improve json feed layout

--- src/_includes/layouts/feed.json.ts
+++ src/_includes/layouts/feed.json.ts
@@ -1,9 +1,11 @@
+import { type FFFFlavoredFrontmatter, toJSONFeedItem } from 'fff'
+
 /** @see {@link https://github.com/lumeland/lume/blob/main/plugins/feed.ts} */
 const clean = (obj: Record<string, unknown>) =>
   Object.fromEntries(
-    Object.entries(obj).filter(([, value]) =>
-      value !== undefined && value !== null
-    ),
+    Object.entries(obj)
+      .filter(([, value]) => value !== undefined && value !== null)
+      .sort(([keyA], [keyB]) => keyA.localeCompare(keyB)),
   )
 
 export default (
@@ -11,9 +13,9 @@ export default (
   { url }: Lume.Helpers,
   // { md, url, htmlUrl }: Lume.Helpers,
 ) =>
-  JSON.stringify(
-    clean({
-      version: 'https://jsonfeed.org/version/1.1',
+  JSON.stringify({
+    version: 'https://jsonfeed.org/version/1.1',
+    ...clean({
       title: data.site.title,
       home_page_url: url('', true),
       feed_url: url(data.url, true),
@@ -21,22 +23,19 @@ export default (
         ? url(data.pagination.next, true)
         : undefined,
       description: data.site?.description,
-      items: data.results?.map((result) => {
-        const item = result as unknown as Lume.Data
-        return clean({
-          // TOOD: fix toJSONFeedItem
-          // ...toJSONFeedItem(item as FFFFlavoredFrontmatter),
-          id: url(item.url, true),
-          url: url(item.url, true),
-          date_published: new Date(item.published ?? item.created ?? item.date)
-            .toISOString(),
-          date_modified: new Date(
-            item.updated ?? item.published ?? item.created ?? item.date,
-          ).toISOString(),
-          // content_html: item.content
-          //   ? htmlUrl(md(item.content as string), true)
-          //   : undefined,
-        })
-      }) as object[],
     }),
-  )
+    items: data.results?.map((item) =>
+      clean(toJSONFeedItem(item as FFFFlavoredFrontmatter, {
+        id: url(item.url, true),
+        url: url(item.url, true),
+        // date_published: new Date(item.published ?? item.created ?? item.date)
+        //   .toISOString(),
+        // date_modified: new Date(
+        //   item.updated ?? item.published ?? item.created ?? item.date,
+        // ).toISOString(),
+        // content_html: item.content
+        //   ? htmlUrl(md(item.content as string), true)
+        //   : undefined,
+      }))
+    ) as object[],
+  })
