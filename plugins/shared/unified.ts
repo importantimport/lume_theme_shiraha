6ec8a4d71aaac0f461d6148fcf0b78d5aebcb739
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-14 23:09:50 +0800

    chore: add rehype slug

--- plugins/shared/unified.ts
+++ plugins/shared/unified.ts
@@ -3,6 +3,7 @@ import type { PluggableList } from 'lume/deps/remark.ts'
 import remarkFFF from 'npm:remark-fff@1.2.1'
 import remarkSmartyPants from 'npm:remark-smartypants@2.1.0'
 // rehype plugins
+import rehypeSlug from 'npm:rehype-slug@5.1.0'
 import rehypeAutolinkHeadings from 'npm:rehype-autolink-headings@7.1.0'
 import rehypePrettyCode from 'npm:rehype-pretty-code@0.12.1'
 // shikiji transformers
@@ -21,6 +22,8 @@ export const remarkPlugins: PluggableList = [
 ]
 
 export const rehypePlugins: PluggableList = [
+  // deno-lint-ignore no-explicit-any
+  rehypeSlug as any,
   rehypeAutolinkHeadings,
   [
     rehypePrettyCode,
