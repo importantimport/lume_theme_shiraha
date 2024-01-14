cb22f63717d0eafb29e50d70bad4b66b716c0d93
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-14 22:25:21 +0800

    fix(feed.json): content html

--- src/_includes/layouts/feed.json.ts
+++ src/_includes/layouts/feed.json.ts
@@ -9,10 +9,9 @@ const clean = (obj: Record<string, unknown>) =>
       .sort(([keyA], [keyB]) => keyA.localeCompare(keyB)),
   )
 
-export default (
+export default async (
   data: Lume.Data,
-  { url }: Lume.Helpers,
-  // { md, url, htmlUrl }: Lume.Helpers,
+  { md, url, htmlUrl }: Lume.Helpers,
 ) =>
   JSON.stringify({
     version: 'https://jsonfeed.org/version/1.1',
@@ -25,18 +24,24 @@ export default (
         : undefined,
       description: data.site?.description,
     }),
-    items: data.results?.map((item) =>
-      clean(toJSONFeedItem(item as FFFFlavoredFrontmatter, {
-        id: url(item.url, true),
-        url: url(item.url, true),
-        // date_published: new Date(item.published ?? item.created ?? item.date)
-        //   .toISOString(),
-        // date_modified: new Date(
-        //   item.updated ?? item.published ?? item.created ?? item.date,
-        // ).toISOString(),
-        // content_html: item.content
-        //   ? htmlUrl(md(item.content as string), true)
-        //   : undefined,
-      }))
-    ) as object[],
+    items: await Promise.all(
+      data.results?.map(async (item) =>
+        clean({
+          ...toJSONFeedItem(item as FFFFlavoredFrontmatter, {
+            id: url(item.url, true),
+            url: url(item.url, true),
+            content_html: item.content
+              ? htmlUrl(await md(item.content as string), true)
+              : undefined,
+          }),
+          date_published: new Date(
+            item.published ?? item.created ?? item.date,
+          )
+            .toISOString(),
+          date_modified: item.updated
+            ? new Date(item.updated).toISOString()
+            : undefined,
+        })
+      ) as object[],
+    ),
   })
