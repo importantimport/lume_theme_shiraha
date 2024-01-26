6a5623b9141c5a715a8513fb0d40d5ed047ff048
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-26 19:59:10 +0800

    refactor(plugins/lts/theme): try catch

--- plugins/lts/theme.ts
+++ plugins/lts/theme.ts
@@ -1,4 +1,4 @@
-import { Page } from 'lume/core/file.ts'
+import { type Data, Page } from 'lume/core/file.ts'
 import { read } from 'lume/core/utils/read.ts'
 import { merge } from 'lume/core/utils/object.ts'
 
@@ -25,6 +25,31 @@ export default (userOptions?: Partial<Options>) => (site: Lume.Site) => {
 
   const defaultTheme = themeFromHex(options.color)
 
+  const applyDefaultTheme = (page: Page<Data>) => {
+    const link = page.document!.createElement('link')
+    link.setAttribute('rel', 'stylesheet')
+    link.setAttribute('href', site.url(options.filename))
+    page.document!.head.appendChild(link)
+
+    const themeColorLight = page.document!.createElement('meta')
+    themeColorLight.setAttribute('name', 'theme-color')
+    themeColorLight.setAttribute(
+      'content',
+      hexFromArgb(defaultTheme.light.primaryContainer),
+    )
+    themeColorLight.setAttribute('media', '(prefers-color-scheme: light)')
+    page.document!.head.appendChild(themeColorLight)
+
+    const themeColorDark = page.document!.createElement('meta')
+    themeColorDark.setAttribute('name', 'theme-color')
+    themeColorDark.setAttribute(
+      'content',
+      hexFromArgb(defaultTheme.dark.primaryContainer),
+    )
+    themeColorDark.setAttribute('media', '(prefers-color-scheme: dark)')
+    page.document!.head.appendChild(themeColorDark)
+  }
+
   site.addEventListener('beforeRender', ({ pages }) => {
     const tokens = Page.create({
       url: options.filename,
@@ -43,54 +68,42 @@ export default (userOptions?: Partial<Options>) => (site: Lume.Site) => {
 
         // TODO: FFF Object Media
         if (page.data.image) {
-          const image = await read(page.data.image, true)
-          const color = sourceColorFromImage(image)
-          const theme = themeFromArgb(color)
+          try {
+            const image = await read(page.data.image, true)
+            const color = sourceColorFromImage(image)
+            const theme = themeFromArgb(color)
 
-          const style = document.createElement('style')
-          style.innerHTML = cssFromTheme(theme)
-          page.document!.head.appendChild(style)
+            const style = document.createElement('style')
+            style.innerHTML = cssFromTheme(theme)
+            page.document!.head.appendChild(style)
 
-          const themeColorLight = document.createElement('meta')
-          themeColorLight.setAttribute('name', 'theme-color')
-          themeColorLight.setAttribute(
-            'content',
-            hexFromArgb(theme.light.primaryContainer),
-          )
-          themeColorLight.setAttribute('media', '(prefers-color-scheme: light)')
-          page.document!.head.appendChild(themeColorLight)
+            const themeColorLight = document.createElement('meta')
+            themeColorLight.setAttribute('name', 'theme-color')
+            themeColorLight.setAttribute(
+              'content',
+              hexFromArgb(theme.light.primaryContainer),
+            )
+            themeColorLight.setAttribute(
+              'media',
+              '(prefers-color-scheme: light)',
+            )
+            page.document!.head.appendChild(themeColorLight)
 
-          const themeColorDark = document.createElement('meta')
-          themeColorDark.setAttribute('name', 'theme-color')
-          themeColorDark.setAttribute(
-            'content',
-            hexFromArgb(theme.dark.primaryContainer),
-          )
-          themeColorDark.setAttribute('media', '(prefers-color-scheme: dark)')
-          page.document!.head.appendChild(themeColorDark)
+            const themeColorDark = document.createElement('meta')
+            themeColorDark.setAttribute('name', 'theme-color')
+            themeColorDark.setAttribute(
+              'content',
+              hexFromArgb(theme.dark.primaryContainer),
+            )
+            themeColorDark.setAttribute('media', '(prefers-color-scheme: dark)')
+            page.document!.head.appendChild(themeColorDark)
+            // } catch (err) {
+            // console.error(err)
+          } catch {
+            applyDefaultTheme(page)
+          }
         } else {
-          const link = document.createElement('link')
-          link.setAttribute('rel', 'stylesheet')
-          link.setAttribute('href', site.url(options.filename))
-          page.document!.head.appendChild(link)
-
-          const themeColorLight = document.createElement('meta')
-          themeColorLight.setAttribute('name', 'theme-color')
-          themeColorLight.setAttribute(
-            'content',
-            hexFromArgb(defaultTheme.light.primaryContainer),
-          )
-          themeColorLight.setAttribute('media', '(prefers-color-scheme: light)')
-          page.document!.head.appendChild(themeColorLight)
-
-          const themeColorDark = document.createElement('meta')
-          themeColorDark.setAttribute('name', 'theme-color')
-          themeColorDark.setAttribute(
-            'content',
-            hexFromArgb(defaultTheme.dark.primaryContainer),
-          )
-          themeColorDark.setAttribute('media', '(prefers-color-scheme: dark)')
-          page.document!.head.appendChild(themeColorDark)
+          applyDefaultTheme(page)
         }
       }))
     },
