73074497cf5772bb918efca0324315ca4f657aa7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-26 20:54:32 +0800

    chore: update cms config

--- _cms.ts
+++ _cms.ts
@@ -1,15 +1,20 @@
 import lumeCMS from 'lume/cms.ts'
 
-export default lumeCMS()
-  .upload('uploads', 'src:public')
-  // TODO:
-  // .document('Settings', 'src:_data.yml', [])
-  .collection('Articles', 'src:articles/*.md', [
-    'title: text',
-    // 'date: date',
-    'summary: text',
-    // 'author: text',
-    'draft: checkbox',
-    'tags: list',
-    'content: markdown',
-  ])
+const cms = lumeCMS()
+
+export const ltsCMS = (lumeCMS: typeof cms) =>
+  lumeCMS
+    .upload('uploads', 'src:public')
+    // TODO:
+    // .document('Settings', 'src:_data.yml', [])
+    .collection('Articles', 'src:articles/*.md', [
+      'title: text',
+      // 'date: date',
+      'summary: text',
+      // 'author: text',
+      'draft: checkbox',
+      'tags: list',
+      'content: markdown',
+    ])
+
+export default ltsCMS(lumeCMS())
