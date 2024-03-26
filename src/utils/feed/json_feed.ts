8cfd375191f1f476608e4f161040aac17ac68419
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-26 19:43:53 +0800

    refactor: move json feed to utils

---
+++ src/utils/feed/json_feed.ts
@@ -0,0 +1,46 @@
+import { toJSONFeedItem } from '../../../deps/fff.ts'
+
+/** @see {@link https://github.com/lumeland/lume/blob/main/plugins/feed.ts} */
+export const clean = (obj: Record<string, unknown>) =>
+  Object.fromEntries(
+    Object.entries(obj)
+      .filter(([, value]) => value !== undefined && value !== null)
+      .sort(([keyA], [keyB]) => keyA.localeCompare(keyB)),
+  )
+
+/** @see {@link https://github.com/lumeland/lume/blob/main/plugins/feed.ts} */
+export const generateJson = async (
+  data: Lume.Data,
+  { md, url, htmlUrl }: Lume.Helpers,
+) =>
+  JSON.stringify(clean({
+    version: 'https://jsonfeed.org/version/1.1',
+    title: data.lts.site.title,
+    home_page_url: url('', true),
+    feed_url: url(data.url, true),
+    next_url: data.pagination?.next
+      ? url(data.pagination.next, true)
+      : undefined,
+    description: data.lts.site.description,
+    authors: [data.lts.author],
+    items: await Promise.all(
+      data.results?.map(async (item) =>
+        clean({
+          ...toJSONFeedItem(item, {
+            id: url(item.url, true),
+            url: url(item.url, true),
+            content_html: item.content
+              ? htmlUrl(await md(item.content as string), true)
+              : undefined,
+          }),
+          date_published: new Date(
+            item.published ?? item.created ?? item.date,
+          )
+            .toISOString(),
+          date_modified: item.updated
+            ? new Date(item.updated).toISOString()
+            : undefined,
+        })
+      ) as object[],
+    ),
+  }))
