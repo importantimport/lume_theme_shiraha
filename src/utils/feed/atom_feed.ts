2ce1083b3dd5dcaba304653a12c7accc79eb6c7f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-26 19:56:59 +0800

    fix(atom_feed): author

--- src/utils/feed/atom_feed.ts
+++ src/utils/feed/atom_feed.ts
@@ -5,9 +5,13 @@ import { generateObject } from './json_feed.ts'
 export const generateAtom = async (
   data: Lume.Data,
   helpers: Lume.Helpers,
-) =>
-  jsonfeedToAtom({
-    ...await generateObject(data, helpers),
+) => {
+  const json = await generateObject(data, helpers)
+
+  return jsonfeedToAtom({
+    ...json,
     /** {@link https://github.com/bcomnes/jsonfeed-to-atom/issues/28} */
     version: 'https://jsonfeed.org/version/1',
+    author: (json.authors as unknown[])[0],
   })
+}
