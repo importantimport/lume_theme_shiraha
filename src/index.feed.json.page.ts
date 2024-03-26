7b65b4eec6d0aeed33d3a5faad96f7d7df8b8bda
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-26 19:31:16 +0800

    fix: exclude unlisted post

--- src/index.feed.json.page.ts
+++ src/index.feed.json.page.ts
@@ -1,6 +1,8 @@
 export const layout = 'layouts/feed.json.ts'
 export default function* ({ search, paginate }: Lume.Data) {
   const pages = search.pages('type=article', 'date=desc')
+    .filter((page) => !page.tags.includes('unlisted'))
+
   for (
     const page of paginate(pages, {
       url: (n) => (n === 1 ? '/feed.json' : `/p/${n}/feed.json`),
--- src/index.page.ts
+++ src/index.page.ts
@@ -1,6 +1,8 @@
 export const layout = 'layouts/article-list.tsx'
 export default function* ({ search, paginate }: Lume.Data) {
   const pages = search.pages('type=article', 'date=desc')
+    .filter((page) => !page.tags.includes('unlisted'))
+
   for (
     const page of paginate(pages, {
       url: (n) => (n === 1 ? '/' : `/p/${n}/`),
