79f770c4eb62723d2115fc93a131d529df38e55e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-25 21:43:02 +0800

    chore: simplify footer

--- src/_components/footer.tsx
+++ src/_components/footer.tsx
@@ -1,7 +1,7 @@
 export default ({ lts }: Lume.Data, {}: Lume.Helpers) => (
   <footer class="h-12 mx-auto flex gap-1 place-items-center">
-    <span>© 2023</span>
-    <a href={lts.author.url}>{lts.author.name}</a>
+    <span>© 2023 {lts.author.name}</span>
+    <span> · </span>
     <span>Powered by</span>
     <a href="https://github.com/importantimport/lume_theme_shiraha">Shiraha</a>
     <span>&</span>
