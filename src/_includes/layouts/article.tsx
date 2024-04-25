fe659a9923ae0c5fc04e3302c07affdffb7de1c2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-25 22:29:33 +0800

    fix(article): set rel=author

--- src/_includes/layouts/article.tsx
+++ src/_includes/layouts/article.tsx
@@ -2,8 +2,8 @@ export const layout = 'layouts/body.tsx'
 export default ({ children, comp: { Container, Tags }, date, title, url: dataUrl, readingInfo, tags }: Lume.Data, { url, slug }: Lume.Helpers) => (
   <Container className="h-entry">
     <article>
-      <a class="u-author" href={url('/')}></a>
       <a class="u-url" value={url(dataUrl, true)}></a>
+      <a class="u-author" rel="author" href={url('/')}></a>
       <h1 class="p-name" style={`--name: article-title-${slug(dataUrl)}`}>{title}</h1>
       <p class="lts-date" style={`--name: article-date-${slug(dataUrl)}`}>
         <time class="dt-published" datetime={date.toISOString()}>
