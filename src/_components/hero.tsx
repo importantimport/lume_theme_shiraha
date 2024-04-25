b8a710efc6720ed5fcab9a7d9828a0641988167c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-25 22:20:05 +0800

    fix: u-photo in h-card

--- src/_components/hero.tsx
+++ src/_components/hero.tsx
@@ -1,9 +1,9 @@
 export default ({ lts }: Lume.Data, { url }: Lume.Helpers) => (
   <div class="p-author h-card flex flex-col gap-4 children:m-0">
-    {/* <data class="u-photo hidden" value={lts.author.avatar} /> */}
+    <data class="u-photo" value={lts.author.avatar} />
     <data class="p-name" value={lts.author.name} />
     <data class="u-uid u-url" value={url('/')} />
-    <img class="u-photo h-16 w-16 rounded-full" src={lts.author.avatar} alt={lts.author.name} />
+    <img class="h-16 w-16 rounded-full" src={lts.author.avatar} alt={lts.author.name} />
     <h1>{lts.site.description}</h1>
     {lts.author.bio && (<p class="p-note color-on-surface opacity-38">{lts.author.bio}</p>)}
     {lts.author.links && (
