e1c0371815c32a8ef697f1e8c11cfb012c447ba3
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-26 17:17:02 +0800

    fix(footer): update year

--- src/_components/footer.tsx
+++ src/_components/footer.tsx
@@ -1,6 +1,6 @@
 export default ({ lts }: Lume.Data, {}: Lume.Helpers) => (
   <footer class="h-12 mx-auto flex gap-1 place-items-center motion-safe:view-transition-footer">
-    <span>© 2023 {lts.author.name}</span>
+    <span>© {new Date().getUTCFullYear()} {lts.author.name}</span>
     <span> · </span>
     <span>Powered by</span>
     <a href="https://github.com/importantimport/lume_theme_shiraha">Shiraha</a>
