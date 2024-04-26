ba8e0c9b0e8649f1b11c151a8bd7a9f2c31d1637
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-26 10:27:53 +0800

    refactor: simplify styles

--- src/_components/footer.tsx
+++ src/_components/footer.tsx
@@ -1,5 +1,5 @@
 export default ({ lts }: Lume.Data, {}: Lume.Helpers) => (
-  <footer class="h-12 mx-auto flex gap-1 place-items-center">
+  <footer class="h-12 mx-auto flex gap-1 place-items-center motion-safe:view-transition-footer">
     <span>© 2023 {lts.author.name}</span>
     <span> · </span>
     <span>Powered by</span>
--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -1,5 +1,5 @@
 export default ({}: Lume.Data, { url }: Lume.Helpers) => (
-  <header class="bg-surface-container w-full rounded-full px-4 h-12 flex place-items-center gap-4">
+  <header class="bg-surface-container w-full rounded-full px-4 h-12 flex place-items-center gap-4 motion-safe:view-transition-header">
     <a href={url('/')}><div class="i-material-symbols:home w-6 h-6" /></a>
     <div class="flex-1"></div>
   </header>
