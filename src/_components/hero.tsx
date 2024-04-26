e1f213de9fa24691a3f1da2d4d5bb3cce1548e81
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-26 10:30:12 +0800

    chore(hero): adjust opacity

--- src/_components/hero.tsx
+++ src/_components/hero.tsx
@@ -5,11 +5,11 @@ export default ({ lts }: Lume.Data, { url }: Lume.Helpers) => (
     <data class="u-uid u-url hidden" value={url('/')} />
     <img class="h-16 w-16 rounded-full" src={lts.author.avatar} alt={lts.author.name} />
     <h1>{lts.site.description}</h1>
-    {lts.author.bio && (<p class="p-note color-on-surface opacity-38">{lts.author.bio}</p>)}
+    {lts.author.bio && (<p class="p-note color-on-surface-variant opacity-72">{lts.author.bio}</p>)}
     {lts.author.links && (
       <div class="flex gap-4">
         {lts.author.links.map(({ icon, href }) => (
-          <a href={href}><div class={`${icon ?? 'i-material-symbols:link'} w-6 h-6 transition color-on-surface opacity-38 hover:opacity-50`} /></a>
+          <a href={href}><div class={`${icon ?? 'i-material-symbols:link'} w-6 h-6 transition color-on-surface-variant opacity-72 hover:opacity-100`} /></a>
         ))}
       </div>
     )}
