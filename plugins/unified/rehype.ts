63bf7b762461fa551a50d5b9111879733c5cfbac
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-09 21:47:58 +0800

    feat(rehype): try transformer copy button

--- plugins/unified/rehype.ts
+++ plugins/unified/rehype.ts
@@ -5,6 +5,7 @@ import rehypePrettyCode from 'npm:rehype-pretty-code@0.13.2'
 // shiki transformers
 import { transformerNotationDiff } from 'npm:@shikijs/transformers@1.10.3'
 import { transformerTwoslash } from 'npm:@shikijs/twoslash@1.10.3'
+import { transformerCopyButton } from 'npm:@rehype-pretty/transformers@0.13.2'
 
 export const rehypePlugins: PluggableList = [
   // deno-lint-ignore no-explicit-any
@@ -27,6 +28,7 @@ export const rehypePlugins: PluggableList = [
           cache: undefined,
           explicitTrigger: true,
         }),
+        transformerCopyButton(),
       ],
     },
   ],
